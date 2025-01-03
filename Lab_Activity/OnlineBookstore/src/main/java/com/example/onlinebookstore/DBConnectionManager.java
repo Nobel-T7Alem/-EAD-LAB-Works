649268623ff5d1b8a18fb3f719e14b80e8f335e1
Nobel Tibebe <alemayehu.nobel.3@gmail.com>
2025-01-03 16:00:32 +0300

    lab exam

---
+++ Lab_Activity/OnlineBookstore/src/main/java/com/example/onlinebookstore/DBConnectionManager.java
@@ -0,0 +1,32 @@
+package com.example.onlinebookstore;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class DBConnectionManager {
+    private String url = "jdbc:mysql://localhost:3306/BookstoreDB";
+    private String username = "root";
+    private String password = "password";
+
+    public Connection openConnection() throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+
+    public void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
